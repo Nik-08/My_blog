5b03f5c222e901688b1cff144f8cc0882e05da84
Nik-08 <69086824+Nik-08@users.noreply.github.com>
2020-09-18 20:37:49 +0700

    feat

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,16 @@
-window.addEventListener("scroll", function(){
-  var header = document.querySelector("header");
-  header.classList.toggle("sticky", window.scrollY > 0);
-})
+// window.addEventListener("scroll", function(){
+//   var nav = document.querySelector("nav");
+//   header.classList.toggle("sticky", window.scrollY > 0);
+// });
+
+
+
+
+
+
+
+
+
+
+
+
