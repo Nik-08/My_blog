2ffd32bff86e03f1ce445948de88cacabe24f73f
Nik-08 <69086824+Nik-08@users.noreply.github.com>
2020-10-13 02:57:03 +0700

    feat(13.10.2020) card-skill

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,15 @@
-// window.addEventListener("scroll", function(){
-//   var nav = document.querySelector("nav");
-//   header.classList.toggle("sticky", window.scrollY > 0);
-// });
 
-
-
-
-
-
-
-
-
-
-
-
+const persent = document.querySelectorAll('.percent');
+  persent.forEach(item => {    
+    const circle = document.querySelectorAll('.circle');
+    h2 = item.querySelectorAll('.percent-value');
+    h2.forEach(elem => {
+      circle.forEach(i => {
+        let str = elem.textContent;      
+        let num = Number(str);
+        if(i.id == elem.id){
+          i.style.strokeDashoffset = `calc(440 - (440 * ${num}) / 100)`;
+        }        
+      });
+    });
+  });
